e45aca05dc69bb872025dec53c4baca900efbb4d
Yoel Susanto <yoelsusantopro@yahoo.com>
2019-03-26 14:39:32 +0700

    Add file reading and add input file

--- chessBoard.py
+++ chessBoard.py
@@ -1,64 +1,19 @@
-#!/usr/bin/python
-# adapted from: 
-#   http://wordaligned.org/articles/drawing-chessboards
 from PIL import Image, ImageDraw
 from itertools import cycle
 
-# def draw_chessboard(n=8, pixel_width=250):
-#     """
-#     Draw an n x n chessboard using PIL.
-#     """
-#     def sq_start(i):
-#         """
-#         Return the square corners, suitable for use in PIL drawings
-#         """
-#         return i*pixel_width / n
+def drawMaze(maxWindowSize, fileName):
+    fileObj = open(fileName,'r')
+    listMaze = [list(line[0:-1]) for line in fileObj]
+    row = len(listMaze)
+    column = len(listMaze[0])
 
-#     def square(i, j):
-#         """
-#         Return the square corners, suitable for use in PIL drawing
-#         """
-#         return map(sq_start, [i, j, i+1, j+1])
-    
-#     image = Image.new("RGB", (pixel_width, pixel_width))
-#     draw_square = ImageDraw.Draw(image).rectangle
-#     squares = (square(i,j)
-#                 for i_start, j in zip(cycle((0, 1)), range(n))
-#                 for i in range(i_start, n, 2))
-#     # squaress = ((i_start, j) for i_start, j in zip(cycle((0, 1)), range(n)) for i in range(i_start, n, 2))
-#     # print(squaress)
-#     # print(squares)
-
-#     for sqs in squaress:
-#         print(sqs)
-#     for sq in squares:
-#         # print(sq)
-#         draw_square(sq, fill='white')
-#     image.show("chessboard.png")
-
-def drawMaze(row, column, boxSize):
-    listMaze = [
-    [1,1,1,1,1,1,1,1,1,1,1],
-    [0,0,0,0,1,0,0,0,0,0,1],
-    [1,1,1,0,1,0,1,1,1,0,1],
-    [1,0,0,0,1,0,1,0,0,0,1],
-    [1,0,1,1,1,0,1,0,1,1,1],
-    [1,0,1,0,0,0,1,0,0,0,1],
-    [1,0,1,0,1,0,1,0,1,0,1],
-    [1,0,1,0,1,0,1,0,1,0,1],
-    [1,0,1,0,1,0,1,0,1,0,1],
-    [1,0,0,0,1,0,1,0,1,0,0],
-    [1,1,1,1,1,1,1,1,1,1,1]
-]
-    """
-    Draw an n x n chessboard using PIL.
-    """
-
+    boxSize = maxWindowSize // max(row,column)
     def calcSqCoor(nth):
         return nth*boxSize
     
     def square(i, j):
         return map(calcSqCoor,[i, j, i+1, j+1])
+
     # width, height
     image = Image.new("RGB", (column*boxSize, row*boxSize))
     draw_square = ImageDraw.Draw(image).rectangle
@@ -66,23 +21,14 @@ def drawMaze(row, column, boxSize):
     i=0
     j=0
     for sq in squares:
-        # print(sq)
-
-        if listMaze[i][j]==0:
-            draw_square(sq, fill='white')
+        if listMaze[i][j]=='0':
+            draw_square(list(sq), fill='white')
         else:
-            draw_square(sq, fill='black')
+            draw_square(list(sq), fill='black')
         j+=1
-        if j==11:
+        if j==column:
             i+=1
             j=0
     image.show()
-# draw 8 x 8 chess board with 500 pixel as pixel width
-# draw_chessboard(8)
-# for i in range(11):
-#     listRow = []
-#     for j in range(11):
-#         listRow.append(input())
-#         print(listRow)
-#     listMaze.append(listRow)
-drawMaze(11,11,31)
+
+drawMaze(600,'input.txt')
